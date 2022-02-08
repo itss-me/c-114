4525c76f6f0b83d3a2afc10ad47ba536af2569f9
Sandhani <84143853+unisandhani@users.noreply.github.com>
2022-02-08 21:03:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+
+}
+
+
+function setup(){
+
+    canvas = createCanvas(300,300);
+    canvas.center()
+}
+
+function draw(){
+    
+}
+
+function take_snapshot(){
+
+    save('army<3bts.png')
+}
